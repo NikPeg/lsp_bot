0fc9919e979abc0818ebe1164236a5edb1502ec5
NikPeg <peganov.nik@gmail.com>
2025-04-12 19:13:24 +0300

    states

---
+++ app/states/__init__.py
@@ -0,0 +1,9 @@
+from .user_states import UserStates, FacultyData, LearningData, ScheduleData, UserData
+
+__all__ = [
+    'UserStates',
+    'FacultyData',
+    'LearningData',
+    'ScheduleData',
+    'UserData'
+]
