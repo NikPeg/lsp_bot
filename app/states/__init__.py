24ee27f3d6f774ef70b0dcad0f38a097be506b06
NikPeg <peganov.nik@gmail.com>
2025-04-17 17:50:18 +0300

    materials
    all new files
    
    no big files

--- app/states/__init__.py
+++
@@ -1,9 +0,0 @@
-from .user_states import UserStates, FacultyData, LearningData, ScheduleData, UserData
-
-__all__ = [
-    'UserStates',
-    'FacultyData',
-    'LearningData',
-    'ScheduleData',
-    'UserData'
-]
