cfccc3d4da5093213beb654480fae7f5b8212538
NikPeg <peganov.nik@gmail.com>
2025-04-24 01:53:56 +0300

    fixes

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,3 +1,11 @@
-from middlewares.i18n import setup_middleware
+from middlewares.i18n import I18nMiddleware
+
+def setup_middleware(dp):
+    """
+    Устанавливает middleware для диспетчера
+    """
+    # Устанавливаем middleware для всех типов сообщений
+    dp.message.middleware(I18nMiddleware())
+    dp.callback_query.middleware(I18nMiddleware())
 
 __all__ = ['setup_middleware']
